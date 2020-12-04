f1eb8afc6260c4fbc828f57ff564f609c974fcf7
jane baik <janebaik@berkeley.edu>
2020-12-04 12:20:09 -0800

    load up seed, just finished edit

--- cats/app/controllers/cats_controller.rb
+++ cats/app/controllers/cats_controller.rb
@@ -23,6 +23,20 @@ class CatsController < ApplicationController
         end
     end
 
+    def edit 
+        @cat = Cat.find(params[:id])
+        render :edit
+    end
+
+    def update
+        cat = Cat.find(params[:id])
+        if cat.update(cat_params)
+            redirect_to cat_url(cat)
+        else
+            render json: cat.errors.full_messages, status: 422
+        end
+    end
+
     private 
     def cat_params
         params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
